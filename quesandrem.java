a2b66c810d223cd51a6a5ef53c5de62ecc1cfe6a
sknithin06 <nithinsk939@gmail.com>
2021-07-03 22:12:12 +0530

    UC1_num is added

--- quesandrem.java
+++ quesandrem.java
@@ -1,6 +1,17 @@
 package Java;
+import java.util.Scanner;
 public class quesandrem {
 	public static void main(String[] args) {
 		System.out.println("Welcome to BasicCoreProgram");
+		
+		Scanner sc= new Scanner(System.in);   
+		System.out.print("Enter a value of Divisor :");  
+		int divisor = sc.nextInt();
+		System.out.print("Enter a value of Dividend:"); 
+		int dividend = sc.nextInt();
+		int Quotient = dividend / divisor;
+		int Remainder = dividend % divisor;
+		System.out.println("Quotient is:"+Quotient);
+		System.out.println("Remainder is:"+Remainder);
 	}
 }
